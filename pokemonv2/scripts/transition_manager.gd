22cab4e6233d8aa390f23dfa31fe72260b5eebf1
Diemisson Lima <diemisson.liima@gmail.com>
2024-10-08 16:51:52 -0400

    corrigido bug de quando vai para proxima cena o mouse fica bloqueado

--- pokemonv2/scripts/transition_manager.gd
+++ pokemonv2/scripts/transition_manager.gd
@@ -3,9 +3,13 @@ extends CanvasLayer
 var new_scene = ""
 
 func fade_to_scene(_new_scene: String) -> void:
+	visible = true
 	new_scene = _new_scene
 	$Animation.play("fade")
 	
 
 func change_scene() -> void:
 	var _change_scene: bool = get_tree().change_scene_to_file(new_scene)
+	
+	await get_tree().create_timer(1.0).timeout
+	visible = false
